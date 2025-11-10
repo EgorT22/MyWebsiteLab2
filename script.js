193b35d0d8da23d62b5961f6878cc3297283e972
Строков Єгор <strokovegor2@gmail.com>
2025-11-10 23:45:12 +0200

    Add website files

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("registerForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value.trim();
+  let surname = document.getElementById("surname").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let password = document.getElementById("password").value.trim();
+
+  if (!name || !surname || !email || !password) {
+    alert("Будь ласка, заповніть усі поля!");
+  } else {
+    alert("Форма успішно відправлена!");
+  }
+});
